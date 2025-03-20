a6d14c41612369b1641a9d4d9a981f6eb6267006
earl-02 <ebering@filamer.edu.ph>
2025-03-20 00:26:11 -0700

    battleship V2 & Functions

---
+++ Getting Functional/function.js
@@ -0,0 +1,44 @@
+// var dogName = "Edgar";
+// var dogWeight = 30;
+// if (dogWeight > 20) {
+//     console.log(dogName + " says WOOF WOOF");
+// } else {
+//     console.log(dogName + " says woof woof")
+// }
+
+// var dogName = "Lark";
+// var dogWeight = 24;
+// if (dogWeight > 20) {
+//     console.log(dogName + " says WOOF WOOF");
+// } else {
+//     console.log(dogName + " says woof woof")
+// }
+
+// var dogName = "Macky";
+// var dogWeight = 18;
+// if (dogWeight > 20) {
+//     console.log(dogName + " says WOOF WOOF");
+// } else {
+//     console.log(dogName + " says woof woof")
+// }
+
+// var dogName = "Blacky";
+// var dogWeight = 15;
+// if (dogWeight > 20) {
+//     console.log(dogName + " says WOOF WOOF");
+// } else {
+//     console.log(dogName + " says woof woof")
+// }
+
+function bark(name, weight){
+    if (weight > 20) {
+        console.log(name + " says WOOF WOOF");
+    } else {
+        console.log(name + " says woof woof")
+    }
+}
+
+bark("Edgar",30);
+bark("Lark",24);
+bark("Macky",18);
+bark("Blacky",15);
---
+++ battleship/battleship_V2.js
@@ -0,0 +1,49 @@
+// Declare Variable
+var randomLoc = Math.floor(Math.random()*5);
+
+var location1 = randomLoc;
+var location2 = location1 + 1;
+var location3 = location2 + 1;
+
+var guess;
+var hits = 0;
+var guesses = 0;
+
+var isSunk = false;
+
+// Game Logic
+
+while(isSunk == false){
+    guess = prompt("Ready!, Aim!, Fire! (Enter number from 0-6)");
+
+        if (guess == null){
+            alert("Thank you for playing!");
+            break;
+        }
+
+        if(guess < 0 || guess > 6){
+            alert("Please enter a valid cell number!");
+
+        }else{
+            guesses += 1;
+
+                if(guess == location1 || guess == location2 || guess == location3){
+                    alert("You Hit!");
+                    hits += 1;
+
+                        if (hits == 3){
+                            isSunk = true;
+                            alert("You sunk my battleship!");
+                        }
+                }else{
+                    alert("Missed");
+                }
+        }
+}
+
+var accuracy = (guesses > 0 ? (hits / guesses) * 100 : 0).toFixed(2);
+
+var stats = "You took " + guesses + " guesses to sink the battleship! " + "\n" + 
+"Accuracy: " + accuracy + "%";
+
+alert(stats);
